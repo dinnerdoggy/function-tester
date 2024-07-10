17caadacd9b109097a3525007eba4162dfbbff23
dinnerdoggy <31261276+dinnerdoggy@users.noreply.github.com>
2024-07-10 11:02:00 -0500

    fully coded

---
+++ main.js
@@ -0,0 +1,25 @@
+function functionName(param) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//only change the function on the right side of the equals operator
+let data = functionName(arg)
+
+//do not change code below this line
+let result = document.getElementById("result")
+result.innerHTML = data
